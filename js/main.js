c6ca06f048e52aa55a95c2013b5b1280a410ac57
Tetsuya Hoshino <hoshino.tetsuya@gmail.com>
2014-05-11 13:42:31 +0900

    release

--- js/main.js
+++ js/main.js
@@ -1,49 +1,8 @@
-
-$(document).on('mobileinit', function() {
-  $.mobile.loader.prototype.options.textVisible = true;
-  $.mobile.loader.prototype.options.theme = 'b';
-  $.mobile.loader.prototype.options.html = '<img src="http://www.wings.msn.to/image/wings.jpg" width="200" /><h3>loading now...</h3></span>';
-});
-
-
 $(window).load(function() {
-	
 
 	$("#fotorama").hide();	
 	$("#shop_data").hide();
 	$("#fotorama_info").hide();
-	
-	//nivoSlider初期化
-	/*
-	$('#slider').nivoSlider({
-		randomStart : false,
-		//effect:'random', // 画像切り替え時のアニメーション
-		//slices:15, // For slice animations
-		//boxCols: 8, // For box animations
-		//boxRows: 4, // For box animations
-		animSpeed : 500, // アニメーション速度(ms)
-		pauseTime : 4000, // 画像切り替えまでの時間(ms)
-		//startSlide:0, // 初めに表示する画像位置
-		//directionNav:true, // 前/次ボタンを表示
-		//directionNavHide:true, // マウスホバー時のみdirectionNavを表示
-		//controlNav:true, // コントロールナビの表示
-		//controlNavThumbs:false, // コントロールナビに画像サムネイルを使用
-		//controlNavThumbsFromRel:false, // Use image rel for thumbs
-		//controlNavThumbsSearch: '.jpg', // Replace this with...
-		//controlNavThumbsReplace: '_thumb.jpg', // ...this in thumb Image src
-		//keyboardNav:true, // スライドをキーボードで操作
-		//pauseOnHover:true, // マウスホバー時に切り替えを一時停止
-		//manualAdvance:false, // 自動スライドしない
-		//captionOpacity:0.8, // キャプションの透過度
-		//prevText: 'Prev', // 前ボタンの名前
-		//nextText: 'Next', // 次ボタンの名前
-		//beforeChange: function(){}, // スライド切り替え前のコールバック関数
-		//afterChange: function(){}, // スライド切り替え後のコールバック関数
-		//slideshowEnd: function(){}, // 全ての画像を表示した後のコールバック関数
-		//lastSlide: function(){}, // 最後の画像が表示される後のコールバック関数
-		//afterLoad: function(){} // スライドのロードが完了したときのコールバック関数
-	});
-*/
 
    //responsiveSlider初期化
    $(".rslides").responsiveSlides({
@@ -65,70 +24,9 @@ $(window).load(function() {
   after: function(){}     // Function: After callback
 });
 
-   
-   //windy初期化
-/*   				var $el = $( '#wi-el' ),
-					windy = $el.windy(),
-					allownavnext = false,
-					allownavprev = false;
-
-				$( '#nav-prev' ).on( 'mousedown', function( event ) {
-
-					allownavprev = true;
-					navprev();
-				
-				} ).on( 'mouseup mouseleave', function( event ) {
-
-					allownavprev = false;
-				
-				} );
-
-				$( '#nav-next' ).on( 'mousedown', function( event ) {
-
-					allownavnext = true;
-					navnext();
-				
-				} ).on( 'mouseup mouseleave', function( event ) {
-
-					allownavnext = false;
-				
-				} );
-
-				function navnext() {
-					if( allownavnext ) {
-						windy.next();
-						setTimeout( function() {	
-							navnext();
-						}, 150 );
-					}
-				}
-				
-				function navprev() {
-					if( allownavprev ) {
-						windy.prev();
-						setTimeout( function() {	
-							navprev();
-						}, 150 );
-					}
-				}
-*/
-
 	//ローディング画像を消す
 	$(".loadingWrap").fadeOut();
-   
-   //ラベルのバックグラウンドがマウスオーバーしたときに変更されないようにする
-   $(".label").unbind("mouseenter").unbind("mouseleave");
 });
-
-
-	
-function windy_popup(){
-	$('.windy-demo').bPopup({
-	    easing: 'easeOutBack', //uses jQuery easing plugin
-            speed: 450,
-            transition: 'slideDown'
-	});
-}
 
 function fotorama_popup(){
 	$('#fotorama').bPopup({
@@ -150,12 +48,6 @@ function shop_data_popup(){
             speed: 450,
             transition: 'slideDown',
 	});
-	
-	var fotorama = $('#fotorama_shop')
-      .fotorama({allowfullscreen: true})
-      .data('fotorama');
-
-    fotorama.requestFullScreen();
 }
 
 function fotorama_info_popup(){
@@ -175,76 +67,9 @@ function fotorama_info_popup(){
 
 function fotorama_close(){
 	$('#fotorama').bPopup().close();
-	//$('#fotorama_shop').bPopup().close();
 	$('#fotorama_info').bPopup().close();
 }
 
 function shop_data_close(){
 	$('#shop_data').bPopup().close();
 }
-
-
-
-
-//以下不要////////////////
-// グレーアウト設定
-function glay() {
-	/* グレイパネル作成
-	 -------------------------------------------------------------------------------*/
-	$('<div class="gray_panel" id="gray_panel" onclick="glay_end()"></div>').css({
-		"background" : "#000",
-		"opacity" : "0.5",
-		"width" : "100%",
-		"height" : 99999,
-		"position" : "fixed",
-		"top" : "0",
-		"left" : "0",
-		"display" : "none",
-		"z-index" : "50"
-	}).appendTo($("body"));
-
-
-	// ポップアップの画面中央になる様な位置関係を算出
-	var left_position = ($("body").width() / 2) - ($("#popup").width() / 2);
-	var top_position = 10;
-
-	// グレーアウトをフェードイン
-	$("#gray_panel").fadeIn("slow");
-
-	// ポップアップのスタイルを定義
-	$("#popup")
-		.css("position", "relative")
-		.css("z-index", "51")
-		.css("position", "fixed")
-		.css("top", top_position)
-		.css("left", left_position)
-		.fadeIn("slow");
-
-	// フレックススライダー初期化
-	$('.flexslider').flexslider({
-        animation: "slide",
-        controlNav: "thumbnails"
-      });
-    
-    // フレックススライダーCSS
-    $(".slides")
-    	.css("text-align", "center")
-    	.css("color", "#fff")
-		.css("background-color", "rgba(0, 0, 0, 0.8)");
-
-	// 閉じるボタンCSS
-	$(".menu_close")
-		.css("height", "30px")
-		.css("width", "30px")
-		.css("left", "5px")
-		.css("top", "5px");
-};
-
-function glay_end() {
-	// グレーアウトをフェードアウト
-	$("#gray_panel").fadeOut("slow");
-
-	// ポップアップをフェードアウト
-	$("#popup").fadeOut("slow");
-};
-
